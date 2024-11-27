e980193bcc36d0effab74a8c3f67a66d25b1fb3f
Carlos Augusto <caarlos.frei@gmail.com>
2024-11-27 17:04:25 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import './styles/global.css';
 import Band from "./components/band/Band";
 import Footer from "./components/footer/Footer";
 import Topbar from "./components/topToolBar/TopToolBar";
